3d5738a3d58015ebadd5dfba504c38a160c153ec
Joe Gallegos <joebgallegos@gmail.com>
2020-09-27 17:49:46 -0500

    initial commmit

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion'
+
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return <motion.h1 animate={{ x: 115 }} className="title">{title}</motion.h1>
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,35 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+// import Footer from "@components/Footer";
 
 export default function Home() {
+  let currentYear = new Date().getFullYear()
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Joe Gallegos - Software Engineer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <motion>
+          <Header title="Hi, I'm Joe 👋" />
+          {/* <p className="description">
+            In 2017, I decided to make a career change. Not to say that small
+            business and home mortgage loans aren't fun... I just wanted to start
+            building things. Fast forward to the year {currentYear.valueOf()}, and
+            I'm building complex web and mobile apps.
+          </p> */}
+          <p className="description">
+            You can find me on{" "}
+            <a href="https://linkedin.com/in/joebgallegos">LinkedIn</a> and{" "}
+            <a href="https://github.com/joeblas">GitHub</a>.
+          </p>
+          <code>// TODO: Make site more fun.</code>
+        </motion>
       </main>
 
-      <Footer />
+      {/* <Footer /> */}
 
       <style jsx>{`
         .container {
@@ -29,12 +41,14 @@ export default function Home() {
         }
 
         main {
-          padding: 5rem 0;
+          padding: 5rem 30rem;
           flex: 1;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          line-height: 1.5rem;
+          font-size: 1.2rem;
         }
 
         code {
@@ -43,6 +57,10 @@ export default function Home() {
           padding: 0.75rem;
           font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;
         }
+
+        a {
+          text-decoration: none;
+        }
       `}</style>
 
       <style jsx global>{`
@@ -50,8 +68,9 @@ export default function Home() {
         body {
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
-            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
         }
 
         * {
@@ -59,5 +78,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
